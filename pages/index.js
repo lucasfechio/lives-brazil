d1e2d4a200a7159676d444106695511dbac7f78d
felipe okino <felipe1okino@live.com>
2020-04-24 17:14:24 -0300

    abrindo os detalhes da live em outra pagina

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,29 @@
-import React, { useState, useEffect } from "react";
-import Head from "next/head";
-import { db } from "../config/firebase";
+import React, { useEffect, useState } from 'react';
+
+import Head from 'next/head';
+import { db } from '../config/firebase';
 
 export default function Home() {
   const [livesList, setLivesList] = useState([]);
-  const [date, setDate] = useState("");
+  const [date, setDate] = useState('');
 
   useEffect(() => {
     const dateNow = new Date();
-    db.collection("lives")
+
+    db.collection('lives')
       .doc(
         `${dateNow.getDate()}${dateNow.getMonth() + 1}${dateNow.getFullYear()}`
       )
       .get()
-      .then((doc) => setLivesList(doc.data().lives))
-      .catch((err) => console.log("Error getting document", err));
+      .then((doc) =>
+        setLivesList(
+          doc.data().lives.map((live, position) => ({
+            ...live,
+            id: `${dateNow.getTime()}_${position}`,
+          }))
+        )
+      )
+      .catch((err) => console.log('Error getting document', err));
     setDate(
       `${dateNow.getDate()}/${dateNow.getMonth() + 1}/${dateNow.getFullYear()}`
     );
@@ -33,7 +42,7 @@ export default function Home() {
         <p className="description">Lives {date}</p>
         <div className="grid">
           {livesList.map((live) => (
-            <a key={live.name} className="card" href="/details">
+            <a key={live.name} className="card" href={`/details?id=${live.id}`}>
               <img className="live-image" src={live.imageUrl} alt="" />
               <p>{live.name}</p>
             </a>
